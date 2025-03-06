68fa405ce2207a082da727c29c412b576286c46c
Guillaume Roux <rouxguillaume8@gmail.com>
2025-03-06 20:17:32 +0100

    refactor: simplify

--- packages/state_management/lib/state_management.dart
+++ packages/state_management/lib/state_management.dart
@@ -1,5 +1,3 @@
-export 'src/async_value/async_value.dart';
-export 'src/async_value/async_value_builder.dart';
 export 'src/change_notifiers/multi_listenable_notifier.dart';
 export 'src/locator.dart';
 export 'src/mixins/hook.dart';
