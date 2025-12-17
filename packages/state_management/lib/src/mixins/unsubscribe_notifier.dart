743484b53067c09102448046ff9ed8e7c525398e
Guillaume Roux <rouxguillaume8@gmail.com>
2025-12-17 10:30:59 +0100

    doc: improved UnsubscribeNotifier comments

--- packages/state_management/lib/src/mixins/unsubscribe_notifier.dart
+++ packages/state_management/lib/src/mixins/unsubscribe_notifier.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/foundation.dart';
 
-/// Mixin that provides functionality to notify when there are no more
-/// subscribers listening to the notifier.
+/// A [ChangeNotifier] mixin that provides functionality to notify when there
+/// are no more subscribers listening to it.
 mixin UnsubscribeNotifier on ChangeNotifier {
   /// Callback invoked when there are no more subscribers to this notifier.
   VoidCallback? _onUnsubscribed;
   set onUnsubscribed(VoidCallback? callback) => _onUnsubscribed = callback;
 
+  @mustCallSuper
   @override
   void removeListener(VoidCallback listener) {
     super.removeListener(listener);
