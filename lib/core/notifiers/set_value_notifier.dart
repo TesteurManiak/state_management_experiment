f3f15befc02a20035db68b3237deb2f336f22e76
Guillaume Roux <rouxguillaume8@gmail.com>
2026-01-29 08:46:16 +0100

    chore: added comments

--- lib/core/notifiers/set_value_notifier.dart
+++ lib/core/notifiers/set_value_notifier.dart
@@ -2,6 +2,13 @@ import 'dart:collection';
 
 import 'package:flutter/foundation.dart';
 
+/// A [ChangeNotifier] that holds a [Set] as value.
+///
+/// It exposes methods to modify the set, and notifies listeners
+/// whenever the set is modified.
+///
+/// The [value] is an unmodifiable view of the underlying set,
+/// so it cannot be changed directly.
 class SetValueNotifier<T> extends ChangeNotifier
     implements ValueListenable<UnmodifiableSetView<T>> {
   SetValueNotifier(Set<T>? initialData) : _data = initialData ?? {};
@@ -12,22 +19,71 @@ class SetValueNotifier<T> extends ChangeNotifier
   @override
   UnmodifiableSetView<T> get value => _view;
 
+  /// Adds [value] to the set.
+  ///
+  /// Returns `true` if [value] (or an equal value) was not yet in the set.
+  /// Otherwise returns `false` and the set is not changed.
+  ///
+  /// Example:
+  /// ```dart
+  /// final dateTimes = <DateTime>{};
+  /// final time1 = DateTime.fromMillisecondsSinceEpoch(0);
+  /// final time2 = DateTime.fromMillisecondsSinceEpoch(0);
+  /// // time1 and time2 are equal, but not identical.
+  /// assert(time1 == time2);
+  /// assert(!identical(time1, time2));
+  /// final time1Added = dateTimes.add(time1);
+  /// print(time1Added); // true
+  /// // A value equal to time2 exists already in the set, and the call to
+  /// // add doesn't change the set.
+  /// final time2Added = dateTimes.add(time2);
+  /// print(time2Added); // false
+  ///
+  /// print(dateTimes); // {1970-01-01 02:00:00.000}
+  /// assert(dateTimes.length == 1);
+  /// assert(identical(time1, dateTimes.first));
+  /// print(dateTimes.length);
+  /// ```
   bool add(T value) {
     final changed = _data.add(value);
     if (changed) notifyListeners();
     return changed;
   }
 
+  /// Adds all [elements] to this set.
+  ///
+  /// Equivalent to adding each element in [elements] using [add],
+  /// but some collections may be able to optimize it.
+  /// ```dart
+  /// final characters = <String>{'A', 'B'};
+  /// characters.addAll({'A', 'B', 'C'});
+  /// print(characters); // {A, B, C}
+  /// ```
   void addAll(Iterable<T> values) {
     _data.addAll(values);
     notifyListeners();
   }
 
+  /// Removes all elements from the set.
+  /// ```dart
+  /// final characters = <String>{'A', 'B', 'C'};
+  /// characters.clear(); // {}
+  /// ```
   void clear() {
     _data.clear();
     notifyListeners();
   }
 
+  /// Removes [value] from the set.
+  ///
+  /// Returns `true` if [value] was in the set, and `false` if not.
+  /// The method has no effect if [value] was not in the set.
+  /// ```dart
+  /// final characters = <String>{'A', 'B', 'C'};
+  /// final didRemoveB = characters.remove('B'); // true
+  /// final didRemoveD = characters.remove('D'); // false
+  /// print(characters); // {A, C}
+  /// ```
   bool remove(T value) {
     final changed = _data.remove(value);
     if (changed) notifyListeners();
