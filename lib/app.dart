3be0dc19575a8e4d225a08a7b818cf6b34d477dd
Guillaume Roux <rouxguillaume8@gmail.com>
2026-09-22 08:59:25 +0200

    chore: update import

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:state_management/state_management.dart';
-import 'package:state_management_experiment/locale/ui/notifiers/locale_notifier.dart';
+import 'package:state_management_experiment/features/locale/ui/notifiers/locale_notifier.dart';
 
 import 'features/home/ui/pages/home_page.dart';
 import 'features/theme/ui/notifiers/theme_mode_notifier.dart';
